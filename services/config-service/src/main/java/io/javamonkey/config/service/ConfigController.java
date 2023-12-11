6d645680b97b5db4d7890a7ca796c7139cfb7bb0
Chris Caspanello <ccaspanello@gmail.com>
2023-12-11 09:39:33 -0500

    Update ConfigController.java
    Added web URL as a quick hack to get post calls working, should be fixed properly with CORS config

--- services/config-service/src/main/java/io/javamonkey/config/service/ConfigController.java
+++ services/config-service/src/main/java/io/javamonkey/config/service/ConfigController.java
@@ -14,6 +14,7 @@ import org.springframework.web.context.annotation.RequestScope;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestScope
 @RestController
 public class ConfigController {
